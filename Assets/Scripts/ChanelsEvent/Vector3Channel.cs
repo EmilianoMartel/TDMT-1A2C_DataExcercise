89c956f3e27b81c33bfeee2d8c2aa10da4e55ddb
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2024-06-17 11:53:27 -0300

    Implemented Channel

---
+++ Assets/Scripts/ChanelsEvent/Vector3Channel.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EventChannels/DataChannels/Vector3", fileName = "Vecto3DataChannel")]
+public class Vector3Channel : EventChannels<Vector3>{}
